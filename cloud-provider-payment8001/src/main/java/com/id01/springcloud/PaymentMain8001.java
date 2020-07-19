18ebd64fc723c81940e947b0ed9b782820f7ff22
zly <id01qaq@163.com>
2020-07-19 16:33:44 +0800

    启动类上增加mapperscan，扫描对应的mapper

--- cloud-provider-payment8001/src/main/java/com/id01/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/id01/springcloud/PaymentMain8001.java
@@ -1,5 +1,6 @@
 package com.id01.springcloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  @date 2020/7/19 0019 - 15:05
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.id01.springcloud.dao")
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class);
