019e26151e8bbfec99dde336d9b290a0f46e885f
zly <id01qaq@163.com>
2020-07-20 14:06:50 +0800

    服务发现discovery

--- cloud-provider-payment8001/src/main/java/com/id01/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/id01/springcloud/PaymentMain8001.java
@@ -3,6 +3,7 @@ package com.id01.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @MapperScan(basePackages = "com.id01.springcloud.dao")
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class);
--- cloud-provider-payment8001/src/main/java/com/id01/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/id01/springcloud/controller/PaymentController.java
@@ -3,10 +3,14 @@ package com.id01.springcloud.controller;
 import com.id01.springcloud.entities.CommonResult;
 import com.id01.springcloud.entities.Payment;
 import com.id01.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,7 +18,30 @@ import java.util.concurrent.TimeUnit;
  @date 2020/7/8 0008 - 15:09
  */
 @RestController
+@Slf4j
 public class PaymentController {
+    /**
+     * 演示服务发现,配合@EnableDiscoveryClient获取微服务相关信息
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 演示服务发现,配合@EnableDiscoveryClient获取微服务相关信息
+     */
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        log.info("services   =====>   "+services.toString());
+        for (String service : services) {
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            for (ServiceInstance instance : instances) {
+                log.info("detail service info  =====>    "+instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+            }
+        }
+        return this.discoveryClient;
+    }
+
     @Autowired
     private PaymentService paymentService;
 
